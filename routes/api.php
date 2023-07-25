18e62953a3f3078f67b1dac4a8e3de2c7cecde09
MOHDHAZIZI <82504629+MOHDHAZIZI@users.noreply.github.com>
2023-07-25 18:38:04 +0800

    login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,10 +17,15 @@ use App\Http\Controllers\SettingsController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
 });
 
 Route::group(['prefix' => 'settings'], function () {
     Route::post('/insertOrupdate', [SettingsController::class, 'store']);
+    Route::get('/list', [SettingsController::class, 'getSettingList']);
 });
