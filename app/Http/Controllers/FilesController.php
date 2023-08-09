9f9296d5f17c8abaf68f80755578462207b80ca2
f41zf4u21 <f41zf4u21@gmail.com>
2023-08-09 16:08:44 +0800

    FM-15

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -9,11 +9,52 @@ use Illuminate\Support\Facades\DB;
 
 class FilesController extends Controller
 {
-    public function fileList(Request $request){
+    public function fileList($user_id){
          $fileList = Files::select('*')
-        ->where('user_id', $request->user_id)
+        ->where('user_id', $user_id)
         ->orderBy('file_name','asc')
         ->get();
-        return response()->json(["message" => "File List", 'list' => $fileList, "code" => 200]);
+        return response()->json(["message" => "File List", 'list' => $fileList, 'envURL' => env('APP_URL'), "code" => 200]);
     }
+
+    public function fileStore(Request $request){
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'file' => 'required',
+            'email' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(["message" => $validator->errors(), "code" => 422]);
+        }
+        $files = $request->file('file');
+        $file_name = $files->getClientOriginalName();
+        $isUploaded = upload_file($files, 'backup/' .$request->email); //file, foldername
+        $file_path = $isUploaded->getData()->path;
+        $dataStore = [
+            'user_id' => $request->user_id,
+            'file_name' => $file_name,
+            'file_path' => $file_path,
+        ];
+
+        $dS = Files::create($dataStore);
+
+       return response()->json(["message" => "Record Successfully Saved", "code" => 200]);
+   }
+
+   public function deleteFile(Request $request){
+        $validator = Validator::make($request->all(), [
+            'backup_id' => 'required|integer',
+            'file_path' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(["message" => $validator->errors(), "code" => 422]);
+        }
+
+        unlink('public' .$request->file_path);
+        $fileList = Files::where(
+            ['backup_id' => $request->backup_id]
+        );
+        $fileList->delete();
+        return response()->json(["message" => "Deleted Successfully.", "code" => 200]);
+   }
 }
